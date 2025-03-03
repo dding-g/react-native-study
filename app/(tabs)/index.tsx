96e446fcb91db087c28ab1e816655e1f752314d3
dding-g <area409@gmail.com>
2025-03-03 15:35:39 +0900

    feat: image tutorial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,18 +2,35 @@ import { View, StyleSheet } from "react-native";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState("");
+  const clickPicker = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: false,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].fileName || "");
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
-        <Button label="Use this photo" />
+        <Button
+          theme="primary"
+          label={selectedImage || "선택해주세요"}
+          onPress={clickPicker}
+        />
+        <Button label="Use this photo" onPress={() => {}} />
       </View>
     </View>
   );
